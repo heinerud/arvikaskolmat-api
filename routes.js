29d7e081c7cf3711560ec09dff5f68b49e7da7f9
Joel Heinerud <joel.heinerud@gmail.com>
2018-05-27 13:31:49 +0200

    Use JSON in source instead of parsing

--- routes.js
+++ routes.js
@@ -1,44 +1,22 @@
 const rp = require('request-promise');
-const cheerio = require('cheerio');
 
-const preschool = {
-    uri: 'https://mpi.mashie.com/public/menu/arvika+kommun/5adac478',
-    transform: body => cheerio.load(body)
-};
-
-const school = {
-    uri: 'https://mpi.mashie.com/public/menu/arvika+kommun/916571e2',
-    transform: body => cheerio.load(body)
-};
+const preschool = 'https://mpi.mashie.com/public/menu/arvika+kommun/5adac478'
+const school = 'https://mpi.mashie.com/public/menu/arvika+kommun/916571e2'
 
 function getMenu(options) {
     return rp(options)
-        .then(($) => {
-            header = $('.nav-header--label').text().trim()
-            dates = $.html()
+        .then(body => {
+            data = body
                 .split('\n')
-                .filter(x => x.includes('js-date'))
-                .map(x => x.slice(x.indexOf('js-date') + 9, -2))
-
-            console.log($('#menu-week').text())
-
-            food = $('.day-alternative').text()
-                .split('\n')
-                .map(x => x.trim())
-                .filter(x => x.length > 0)
-                .filter(x => x !== 'Lunch')
-
-            if (dates.length !== food.length) {
-                return { error: 'Array length mismatch' }
-            }
-
-            menu = dates.map((date, i) => {
-                return { 'date': date, 'dish': food[i] }
-            })
-            return { title: header, menu: menu }
+                .filter(x => x.includes('weekData'))
+                .map(x => x.slice(x.indexOf('{')))[0]
+                .trim()
+                .replace(/new Date\((\d+)\)/g, '"$1"')
+            return JSON.parse(data)
         })
         .catch((err) => console.log(err))
 }
+getMenu(school)
 
 var appRouter = function (app) {
     app.get("/school", (req, res, next) => getMenu(school).then(menu => res.send(menu)))
